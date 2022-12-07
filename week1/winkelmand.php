c7053f231d704ddb24637e940c05c9da962cb749
svmcd <spolat0750@gmail.com>
2022-12-07 10:49:52 +0100

    opdracht 1 af

---
+++ week1/winkelmand.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Winkelmand integer</title>
+</head>
+<body>
+
+<?php
+$prijs = 1.25;
+echo 'Prijs: ' . $prijs;
+?>
+
+<h2>Winkelmand</h2>
+<form action="" method="post">
+    <label for="product">Product:</label><br>
+    <input type="text" name="product" required placeholder="product"><br>
+    <label for="aantal">Aantal:</label><br>
+    <input type=number name="aantal" required placeholder="aantal">><br><br>
+    <input type="submit" name="submit" value="toevoegen" >
+</form>
+<p>Vul je geen action in dan wordt het formulier verwerkt door de huidige pagina".</p>
+
+<?php
+
+// Controleer of het formulier is verstuurd
+if ($_POST['submit']) {
+
+    $product = $_POST['product'];
+    $aantal = $_POST['aantal'];
+
+
+    // Controleer of het aantal > 0 is.
+    if ($aantal > 0 && $aantal <= 15) { 
+
+        $prijs_result = $prijs * $aantal;
+        echo "<pre>";
+        echo "gekozen product en totaal prijs\n\nProduct: ", $product, "\n", "Aantal in winkelmand: ", $aantal, "\n", "Totaal prijs: ", $prijs_result;
+        echo "</pre>";
+
+    } else{
+        echo "er mag geen een negatief aantal ingevoerd worden";
+    }
+
+    /*
+     * Werkt je formulier pas dan het formulier aan zodat je geen
+     * negatieve aantallen kunt invullen.
+     * Pas ook het formulier aan zodat je maximaal 15 kunt invoeren
+     * bij het aantal.
+     */
+
+}
+?>
+
+
+</body>
+</html>
